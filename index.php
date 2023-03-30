49b8311843ca89a52679efa91990c01ba10387c2
Atila Sobral <assobraliveira@gmail.com>
2023-03-29 23:57:45 -0300

    PHP

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Título da aba</title>
+    </head>
+    <body>
+        <style>
+            /* tag style é um dos lugares para codificar css*/
+            h1{
+                color: white;
+                background-color: black;
+                text-align: center;
+                border-radius: 0.3cm;
+            }
+        </style>
+
+        <h1 onclick="alert('Olá mundo JS')">Minha Página</h1>
+
+        <script>
+            document.write('Olá mundo JS hehe')
+        </script>
+
+        <?php
+            echo "Olá mundo PHP!" 
+        ?>
+    </body>
+
+
+</html>
