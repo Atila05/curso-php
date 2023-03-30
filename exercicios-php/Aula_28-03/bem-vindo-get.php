47403514429cfd1f9db2cabea0f65b06a1294a7f
Atila Sobral <assobraliveira@gmail.com>
2023-03-30 03:26:04 -0300

    Exercício 29/03

--- exercicios-php/Aula_28-03/bem-vindo-get.php
+++ exercicios-php/Aula_28-03/bem-vindo-get.php
@@ -2,8 +2,10 @@
 
     $nome = $_GET["Nome"];
     $email = $_GET["email"];
+    $idade = $_GET["idade"];
 
-    echo "Bem-vindo, $nome <br>";
+    echo "Bem-vindo, $nome ! <br>";
     echo "E-mail registrado: $email <br>";
+    echo "Sua idade: $idade anos.";
 
 ?>
--- exercicios-php/Aula_28-03/bem-vindo-post.php
+++ exercicios-php/Aula_28-03/bem-vindo-post.php
@@ -1,6 +1,7 @@
 <?php
 
-    echo "Bem-vindo, "; echo $_POST["Nome"]; echo "<br>";
-    echo "E-mail registrado: "; echo $_POST["email"]; echo "<br>";
+    echo "Bem-vindo, "; echo $_POST["Nome"]; echo "! <br>";
+    echo "E-mail registrado: "; echo $_POST["email"]; echo ". <br>";
+    echo "Sua idade: "; echo $_POST["idade"]; echo " anos. <br>";
 
 ?>
